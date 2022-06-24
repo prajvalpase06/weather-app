3af38f68e862e81cb077bd1c14af553deabf54ea
Prajval <aryan.pase@gmail.com>
2022-06-24 13:56:05 +0400

    Updated file to dynamically change temperature, location and details

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -8,47 +8,62 @@ import {
   UilSun,
   UilSunset,
 } from "@iconscout/react-unicons";
+import { formatToLocalTime, iconURL } from '../services/weatherService';
 
-const TemperatureAndDetails = () => {
+
+const TemperatureAndDetails = ({ weather: {
+    details, 
+    icon,
+    temp_min,
+    temp_max,
+    temp,
+    sunrise,
+    sunset,
+    speed,
+    humidity,
+    feels_like,
+    timezone, 
+    wind
+} }) => {
   return (
     <div>
         <div className='items-center justify-center text-xl py-6 flex text-cyan-300'>
-            <p>Clear</p>
+            <p>{details}</p>
         </div>
         <div className='flex flex-row items-center justify-between text-white py-3'>
-            <img src="http://openweathermap.org/img/wn/10d@2x.png" className='w-20'/>
-            <p className='text-5xl'>34°</p>
+            <img src={iconURL(icon)} className='w   -20'/>
+            <p className='text-5xl'>{`${temp.toFixed()}°`}</p>
             <div className='flex-col flex space-y-2'>
                 <div className='flex items-center justify-center'>
                     <UilTemperature size={18} className="mr-1"/>
                     Real fell: 
-                    <span className='ml-1 font-medium'>32°</span>
+                    <span className='ml-1 font-medium'>{`${feels_like.toFixed()}°`}</span>
                 </div>
                 <div className='flex items-center justify-center'>
                     <UilTear size={18} className="mr-1"/>
                     Humidity: 
-                    <span className='ml-1 font-medium'>41%</span>
+                    <span className='ml-1 font-medium'>{`${humidity.toFixed()}%`}</span>
                 </div>
                 <div className='flex items-center justify-center'>
                     <UilWind size={18} className="mr-1"/>
                     Wind: 
-                    <span className='ml-1 font-medium'>4 km/hr</span>
+                    <span className='ml-1 font-medium'>{`${speed.toFixed()} km/hr`}</span>
                 </div>
             </div>
         </div>
 
         <div className='flex flex-row items-center justify-center space-x-1 text-white text-sm py-3'>
             <UilSun />
-            <p className='font-light'>Rise: <span className='font-medium ml-1'>06:00 AM</span></p>
+            <p className='font-light'>Rise: <span className='font-medium ml-1'>{formatToLocalTime(sunrise, timezone, "hh:mm a")}</span></p>
             <p className='font-light text-lg'> | </p>
             <UilSunset />
-            <p className='font-light'>Set: <span className='font-medium ml-1'>06:00 AM</span></p>
+            <p className='font-light'>Set: <span className='font-medium ml-1'>{formatToLocalTime(sunset, timezone, "hh:mm a")}</span></p>
             <p className='font-light text-lg'> | </p>
             <UilArrowUp />
-            <p className='font-light'>High: <span className='font-medium ml-1'>45°</span></p>
+            <p className='font-light'>High: <span className='font-medium ml-1'>{`${temp_max.toFixed()}°`}</span></p>
             <p className='font-light text-lg'> | </p>
             <UilArrowDown />
-            <p className='font-light'>Low: <span className='font-medium ml-1'>22°</span></p>
+            <p className='font-light'>Low: <span className='font-medium ml-1'>{`${temp_min.toFixed()}°`}</span></p>
         </div>
 
     </div>
--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { formatToLocalTime } from '../services/weatherService'
 
-const TimeAndLocation = () => {
+const TimeAndLocation = ({ weather: {dt, timezone, name, country} }) => {
   return (
     <div>
         <div className='flex items-center justify-center my-6'>
             <p className='text-white text-xl font-medium'>
-                Wednesday, 22 June 2022 | Local Time: 1:19 PM
+                {formatToLocalTime(dt, timezone)}
             </p>
         </div>
         <div className='flex items-center justify-center my-3'>
             <p className='text-white text-3xl'>
-                Berlin, DE
+                {`${name}, ${country}`}
             </p>
         </div>
     </div>
